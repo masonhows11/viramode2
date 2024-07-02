f63c9f0698523ac68a97bea0ac58a8e86313f167
mason.hows11 <mason.hows11@gmail.com>
2024-07-02 18:47:20 +0330

    admin panel template updated

---
+++ resources/views/admin/layout/styles.blade.php
@@ -0,0 +1,17 @@
+<link rel="shortcut icon" href="{{ asset('admin_assets/media/logos/favicon.ico') }}" />
+<!--begin::Fonts(mandatory for all pages)-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/cssfamily=Inter:300,400,500,600,700" />
+<!--end::Fonts-->
+<!--begin::Vendor Stylesheets(used for this page only)-->
+<link href="{{ asset('admin_assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet"
+    type="text/css" />
+<link href="{{ asset('admin_assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet"
+    type="text/css" />
+<!--end::Vendor Stylesheets-->
+<!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+<link href="{{ asset('admin_assets/plugins/global/plugins.bundle.rtl.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('admin_assets/css/style.bundle.rtl.css') }}" rel="stylesheet" type="text/css" />
+<!--end::Global Stylesheets Bundle-->
+
+<script>
+// Frame-busting to prevent site from being loaded within a frame without permission (click-jacking) if (window.top != window.self) { window.top.location.replace(window.self.location.href); }</script>
