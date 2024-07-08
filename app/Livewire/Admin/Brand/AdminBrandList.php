5275cc2b83226342bdc560b72d7f54117b202387
mason.hows11 <mason.hows11@gmail.com>
2024-07-08 22:15:29 +0330

    add brand admin section done

--- app/Livewire/Admin/Brand/AdminBrandList.php
+++ app/Livewire/Admin/Brand/AdminBrandList.php
@@ -2,14 +2,78 @@
 
 namespace App\Livewire\Admin\Brand;
 
+use App\Models\Brand;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AdminBrandList extends Component
 {
+    protected $paginationTheme = 'bootstrap';
+    public $search = '';
+    use WithPagination;
+    public $delete_id;
+    public $brand_active = true;
+    public $brand_id;
+
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function active($id)
+    {
+        $this->brand_id = $id;
+
+        $brand = Brand::findOrFail($id);
+        if ($brand->is_active == 0) {
+            $brand->is_active = 1;
+            $this->brand_active = true;
+            $brand->save();
+        } else {
+            $brand->is_active = 0;
+            $this->brand_active = false;
+            $brand->save();
+        }
+        $this->dispatch('show-result',
+            ['type' => 'success',
+                'message' => __('messages.The_changes_were_made_successfully')]);
+
+    }
+
+    public function deleteConfirmation($id)
+    {
+        $this->delete_id = $id;
+        $this->dispatch('show-delete-confirmation');
+    }
+
+    protected $listeners = [
+        'deleteConfirmed' => 'deleteBrand',
+    ];
+
+    public function deleteBrand()
+    {
+        try {
+
+            $brand = Brand::findOrFail($this->delete_id);
+            if($brand->logo_path != null){
+                Storage::disk('public')->delete('/images/'.$brand->logo_path);
+            }
+            $brand->delete();
+            $this->dispatch('show-result', type:'success',message:'رکورد با موفقیت حذف شد');
+        } catch (\Exception $ex) {
+            return view('errors_custom.model_not_found');
+        }
+        return null;
+    }
+
     public function render()
     {
-        return view('livewire.admin.brand.admin-brand-list') 
+        return view('livewire.admin.brand.admin-brand-list')
         ->extends('admin.layout.master_admin')
-        ->section('admin_main');
+        ->section('admin_main')
+            ->with(['brands' => Brand::where('title_persian','like','%'.$this->search.'%')
+                ->OrWhere('title_english','like','%'.$this->search.'%')->orderBy('id','asc')->paginate(9)]);
     }
 }
--- resources/views/livewire/admin/brand/admin-edit-brand.blade.php
+++ resources/views/livewire/admin/brand/admin-edit-brand.blade.php
@@ -1,3 +1,111 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    @section('dash_page_title')
+        ویرایش برند
+    @endsection
+    @section('breadcrumb')
+        {{ Breadcrumbs::render('admin.brands.edit',$brand_title) }}
+    @endsection
+    <div class="container-fluid">
+
+        <form wire:submit.prevent="update">
+            <div class="row  py-2 bg-white rounded">
+                <div class="row">
+
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="mb-3 mt-3">
+                                <label for="title_p" class="form-label">عنوان ( فارسی )</label>
+                                <input wire:model.defer="title_persian" type="text" class="form-control" id="title_p">
+                            </div>
+                            @error('title_persian')
+                            <div class="mt-3">
+                                <span class="text-danger">{{ $message}}</span>
+                            </div>
+                            @enderror
+                            <div class="mb-3">
+                                <label for="title_e" class="form-label">عنوان ( انگلیسی )</label>
+                                <input wire:model.defer="title_english" type="text" dir="ltr" class="form-control"
+                                       id="title_e">
+                            </div>
+                            @error('title_english')
+                            <div class="mt-3">
+                                <span class="text-danger">{{ $message}}</span>
+                            </div>
+                            @enderror
+                            <div class="mb-3">
+                                <label for="seo_desc" class="form-label">توضیحات سئو</label>
+                                <input wire:model.defer="seo_desc" type="text" dir="ltr" class="form-control"
+                                       id="seo_desc">
+                            </div>
+                            @error('seo_desc')
+                            <div class="mt-3">
+                                <span class="text-danger">{{ $message}}</span>
+                            </div>
+                            @enderror
+
+                            <div class="mb-3">
+                                <label for="is_active" class="form-label">وضعیت:</label>
+                                <select wire:model.defer="is_active" class="form-select" id="is_active">
+                                    <option value="1">فعال</option>
+                                    <option value="0">غیر فعال</option>
+                                </select>
+                            </div>
+                            @error('active')
+                            <div class="mt-3">
+                                <span class="text-danger">{{ $message}}</span>
+                            </div>
+                            @enderror
+
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <div class="mb-3 mt-10 d-flex justify-content-center">
+                            @if($logo)
+                                <img src="{{ $logo->temporaryUrl() }}"
+                                     width="200" height="200"
+                                     alt="brand_photo"
+                                     class="rounded border border-2 image-admin-preview">
+                            @else
+                                @if($brand_logo && \Illuminate\Support\Facades\Storage::disk('public')->exists('images/brand/'.$brand_logo ))
+                                    <img src="{{ asset('storage/images/brand/'.$brand_logo) }}"
+                                         width="200" height="200"
+                                         alt="brand_photo"
+                                         class="rounded border border-2 image-admin-preview">
+                                @else
+                                    <img src="{{  asset('admin_assets/images/no-image-icon-23494.png') }}"
+                                         width="200" height="200"
+                                         alt="brand_photo"
+                                         class="rounded border border-2 image-admin-preview">
+                                @endif
+                            @endif
+                        </div>
+
+                        <div class="mb-1 mt-5">
+                            <label for="logo" class="form-label">تصویر برند</label>
+                            <input wire:loading.class="d-none" type="file" class="form-control" wire:model.defer="logo"
+                                   id="logo">
+                        </div>
+                        <div wire:loading wire:target="logo" wire:loading.class="d-flex" class="">
+                            <div class="spinner-border me-2" role="status" aria-hidden="true"></div>
+                            <div><strong>{{ __('messages.uploading')}}</strong></div>
+                        </div>
+                        @error('logo')
+                        <div class="mt-3">
+                            <span class="text-danger">{{ $message}}</span>
+                        </div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-sm-12 mt-4">
+                    <button type="submit" class="btn btn-success">ذخیره</button>
+                    <a href="{{ route('admin.brand.index') }}" class="btn btn-primary">لیست برندها</a>
+                </div>
+
+            </div>
+        </form>
+    </div>
 </div>
+
+
