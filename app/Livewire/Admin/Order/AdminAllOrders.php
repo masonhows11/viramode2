cd41b748f62490026d811efd3a236ece13ab41e0
mason.hows11 <mason.hows11@gmail.com>
2024-07-25 17:58:18 +0330

    refactor all orders updated

--- app/Livewire/Admin/Order/AdminAllOrders.php
+++ app/Livewire/Admin/Order/AdminAllOrders.php
@@ -15,61 +15,67 @@ class AdminAllOrders extends Component
 
     public function changeDeliveryStatus($id)
     {
-        $order = Order::findOrfail($id);
-        switch ($order->delivery_status){
-            case 0:
-                $order->delivery_status = 1;
-                break;
-            case 1:
-                $order->delivery_status = 2;
-                break;
-            case 2:
-                $order->delivery_status = 3;
-                break;
-            default :
-                $order->delivery_status = 0;
-        }
+        $order = Order::findOrFail($id);
+        $order->delivery_status = match ($order->delivery_status) {
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            default => 0,
+        };
+        //        switch ($order->delivery_status){
+        //            case 0:
+        //                $order->delivery_status = 1;
+        //                break;
+        //            case 1:
+        //                $order->delivery_status = 2;
+        //                break;
+        //            case 2:
+        //                $order->delivery_status = 3;
+        //                break;
+        //            default :
+        //                $order->delivery_status = 0;
+        //        }
         $order->save();
-
         $this->dispatch('show-result',type:'success',message:__('messages.The_changes_were_made_successfully'));
 
     }
 
     public function changeOrderStatus($id)
     {
-        $order = Order::findOrfail($id);
-        switch ($order->order_status){
-            case 0:
-                $order->order_status = 1;
-                break;
-            case 1:
-                $order->order_status = 2;
-                break;
-            case 2:
-                $order->order_status = 3;
-                break;
-            case 3:
-                $order->order_status = 0;
-                break;
-            default :
-                $order->order_status = 0;
-        }
+        $order = Order::findOrFail($id);
+        $order->order_status = match ($order->order_status) {
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 0,
+            default => 0,
+        };
+        //        switch ($order->order_status){
+        //            case 0:
+        //                $order->order_status = 1;
+        //                break;
+        //            case 1:
+        //                $order->order_status = 2;
+        //                break;
+        //            case 2:
+        //                $order->order_status = 3;
+        //                break;
+        //            case 3:
+        //                $order->order_status = 0;
+        //                break;
+        //            default :
+        //                $order->order_status = 0;
+        //        }
         $order->save();
-
-        $this->dispatchBrowserEvent('show-result',
-            ['type' => 'success',
-                'message' => __('messages.The_changes_were_made_successfully')]);
+        $this->dispatch('show-result',type:'success',message:__('messages.The_changes_were_made_successfully'));
     }
 
     public function cancelOrder($id)
     {
-        $order = Order::findOrfail($id);
+        $order = Order::findOrFail($id);
         $order->order_status = 4;
         $order->save();
-
-        $this->dispatchBrowserEvent('show-result',
-            ['type' => 'success',
-                'message' => __('messages.The_changes_were_made_successfully')]);
+        $this->dispatch('show-result', type:'success',message:__('messages.The_changes_were_made_successfully'));
     }
 
     public function render()
